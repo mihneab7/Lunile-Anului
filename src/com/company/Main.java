98b901f8420f43283b42d726b00a25186376fbaa
Mihnea <mihneabulearca@gmail.com>
2019-04-23 17:14:20 +0300

    Lunile Anului cu zile

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int month=1, nrZile;
+        String monthName;
+        switch (month) {
+            case 1: monthName="January";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 2: monthName="February";
+            nrZile=28;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 3: monthName="March";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 4: monthName="April";
+            nrZile=30;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 5: monthName="May";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 6: monthName="June";
+            nrZile=30;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 7: monthName="July";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 8: monthName="August";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 9: monthName="September";
+            nrZile=30;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 10: monthName="October";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 11: monthName="November";
+            nrZile=30;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+            case 12: monthName="December";
+            nrZile=31;
+                System.out.println(monthName);
+                System.out.println(nrZile);
+                break;
+            default: monthName="Invalid";
+            System.out.println(monthName);
+            break;
+        }
+	// write your code here
+    }
+}
